80dd48e00554a14905d462ba7fbe1c50d99ec311
Raúl Correia <raulcorreia7@users.noreply.github.com>
2026-01-22 01:21:36 +0100

    feat: add colorscheme plugins

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,7 @@
+return {
+  { "Evalir/dosbox-vim-colorscheme" },
+  { "nyoom-engineering/oxocarbon.nvim" },
+  { "tomasiser/vim-code-dark" },
+  { "Mofiqul/vscode.nvim" },
+  { "aktersnurra/no-clown-fiesta.nvim" },
+}
